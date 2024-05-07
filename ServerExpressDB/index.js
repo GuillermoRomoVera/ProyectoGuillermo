9a6447f1adb687602caeefe3751497d308f3681b
GuillermoRomoVera <you@example.com>
2024-05-07 10:00:41 -0500

    proyecto

--- ServerExpressDB/index.js
+++ ServerExpressDB/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const port = process.env.PORT||3000;
 
 app.use((req,res,next) => {
  console.log("Petición al server")+ new Date();
@@ -15,6 +16,6 @@ app.get("/", (req,res, next) => {
 app.post("/", (req,res, next) => {
     res.send("Hola");
 })
-app.listen(3000, () =>{
-    console.log("Servidor Express escuando en puerto 3000")
+app.listen(port, () =>{
+    console.log(`Servidor Express escuando en puerto ${port}`)
 })
